6ae9c9d187adb9ffa85d684d9a7e47fc4e5e0f3d
Christian Solis <christian@guayaquilenlinea.com>
2015-01-16 17:26:50 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+ /*** error reporting on ***/
+ error_reporting(E_ALL);
+
+ /*** define the site path constant ***/
+ $site_path = realpath(dirname(__FILE__));
+ define ('__SITE_PATH', $site_path);
+
+ /*** include the init.php file ***/
+ include 'includes/init.php';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+</body>
+</html>
